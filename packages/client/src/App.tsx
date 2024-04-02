d67ba2c32e1357aff67f42770b31c8057d121365
tejaboy <lwkbite@gmail.com>
2024-04-02 14:55:50 +0800

    Remove unused images.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -17,33 +17,6 @@ const text = k.add([
 	k.anchor("center")
 ]);
 
-// Load planes
-k.loadSprite("plane_1_Blue", "src/images/plane_1_blue.png");
-k.loadSprite("plane_2_Blue", "src/images/plane_2_blue.png");
-k.loadSprite("plane_3_Blue", "src/images/plane_3_blue.png");
-k.loadSprite("plane_1_Red", "src/images/plane_1_red.png");
-k.loadSprite("plane_2_Red", "src/images/plane_2_red.png");
-k.loadSprite("plane_3_Red", "src/images/plane_3_red.png");
-
-// Load explosion spritesheet
-k.loadSprite("explosion", "src/images/explosion.png", {
-	sliceX: 3,
-	sliceY: 3,
-	anims: {
-		explode: {
-			from: 0,
-			to: 8,
-			speed: 24
-		},
-	},
-});
-
-// Load bullet sprite
-k.loadSprite("bullet1", "src/images/torpedo.png");
-
-// Load logo sprite
-k.loadSprite("logo", "src/images/logo.png");
-
 // Create all scenes
 createLobbyScene();
 
